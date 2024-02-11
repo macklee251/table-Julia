efffb16ae6a51b382e36610fe2e4c87c31259dff
mack lee <114943402+macklee251@users.noreply.github.com>
2024-02-11 19:23:17 -0300

    Pacote pronto e testado

---
+++ dataframe.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.36
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 07092300-c92c-11ee-35aa-a942ccd5fc34
+
+
+# ╔═╡ Cell order:
+# ╠═07092300-c92c-11ee-35aa-a942ccd5fc34
